af4af4749e1d1080d026e4cc063856e70c9dae21
bom318 <kimu_u@naver.com>
2022-12-14 13:24:17 +0900

    글쓰기 유효성검사 js 구현

--- src/main/resources/static/src/writeForm.js
+++ src/main/resources/static/src/writeForm.js
@@ -1,2 +1,18 @@
 'use strict'
 
+const submit = document.querySelector('#write_btn');
+const titleInput = document.querySelector('#boardTitle');
+const contentInput = document.querySelector('#boardContent');
+
+submit.addEventListener('click', (e) => {
+
+    if(titleInput.value == "" ) {
+        e.preventDefault();
+        alert('제목을 입력하세요');
+        titleInput.focus();
+    }else if(contentInput.value == "") {
+        e.preventDefault();
+        alert('내용을 입력하세요');
+        contentInput.focus();
+    }
+})
