443f6178b46eea3a3a181db5c68a5353df7c288d
bom318 <kimu_u@naver.com>
2022-12-14 14:05:09 +0900

    회원가입_중복회원 검사_서버

--- src/main/java/bom/basicboard/service/MemberService.java
+++ src/main/java/bom/basicboard/service/MemberService.java
@@ -11,22 +11,27 @@ import bom.basicboard.repository.Memory.MemoryMemberRepository;
 
 @Service
 public class MemberService {
-    
+
     MemberRepository memberRepository;
-    
+
     @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
-
     public void join(Member member) {
-        //만약 이름 입력 안하면 이름에 아이디 저장
-        if(member.getMemberName() == null || member.getMemberName() == "") {
+        // 만약 이름 입력 안하면 이름에 아이디 저장
+        if (member.getMemberName() == null || member.getMemberName() == "") {
             member.setMemberName(member.getMemberId());
         }
+        // 중복회원 검사
+        Optional<Member> findMember = memberRepository.findByMemberId(member.getMemberId());
+        findMember.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        });
         memberRepository.save(member);
     }
+
     public Member updateMember(Member member, String password, String memberName) {
         Member updateMember = memberRepository.update(member.getUuid(), password, memberName);
         return updateMember;
